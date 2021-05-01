21b99d2bf83d861e41f0b795097829c4d2f160b3
cavalcantif <cavalcanti.filipe@gmail.com>
2021-05-01 17:24:00 +0100

    Added the failedValidation method to the request classes get and conver the errors messages to json.

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Support\Arr;
 
 class UpdateTaskRequest extends FormRequest
 {
@@ -27,5 +30,22 @@ class UpdateTaskRequest extends FormRequest
             'description' => 'required|string|max:255',
             'is_done' => 'required|boolean'
         ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.*
+     * @param Validator $validator
+     * @return array
+     */
+    protected function failedValidation(Validator $validator): array
+    {
+        throw new HttpResponseException(
+            response()->json(
+                [
+                    'errors' => Arr::flatten($validator->errors()->toArray())
+                ],
+                422
+            )
+        );
     }
 }
